cb3f1a68e57de28d49d65181c25a20c68f78a65f
Neil Murphy <barrymun@tcd.ie>
2024-01-13 14:46:00 +0000

    working getManifest, adding web-sys dep

--- scripts/background/src/lib.rs
+++ scripts/background/src/lib.rs
@@ -2,11 +2,16 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 extern "C" {
-    #[wasm_bindgen(js_namespace = console)]
+    #[wasm_bindgen(js_namespace = ["console"])]
     fn log(message: &str);
+
+    #[wasm_bindgen(js_namespace = ["chrome", "runtime"])]
+    fn getManifest() -> JsValue;
 }
 
 #[wasm_bindgen(start)]
 pub fn main() {
-    log("This should appear in the background console")
+    log("This should appear in the background console");
+    let x = getManifest();
+    log(&format!("manifest: {:?}", x));
 }
